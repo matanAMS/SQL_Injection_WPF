8d1b7422e5260de7124f70ad81380e81af15c1ac
Matan Amsalem <me1ten16@gmail.com>
2021-12-16 17:24:16 +0200

    SQLInjection

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SqlInjectionWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static string connectionString = "Data Source=DESKTOP-EDEVO0N\\SQLEXPRESS;Initial Catalog=SqlInjection;Integrated Security=True";
+        private static SqlConnection connection = new SqlConnection(connectionString);
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Server connect = new Server();
+            connect.ConnectDataBase();
+            
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Server select = new Server();
+            select.SelectFromDataBase();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Server insert = new Server();
+            string password = textbox2.Text;
+            string mail = textbox1.Text;
+            if (!insert.CheckIfExist(mail))
+            {
+                insert.InsertToDataBase(mail, password);
+            }
+            else
+                MessageBox.Show("This Mail Is Already Exist in db");
+       
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+         
+        Server update = new Server();
+
+            string mail = textbox1.Text;
+            string password = textbox2.Text;
+            string new_mail = newMail.Text;
+            if (update.CheckIfExist(mail))
+            {
+                update.UpdateData(mail,new_mail, password);
+            }
+            else
+                MessageBox.Show("This Mail Is Already Exist in db");
+
+        }
+
+        private void TextBox_TextChanged_2(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            Server delete = new Server();
+            string mail = DeleteTextBox.Text;
+            if (delete.CheckIfExist(mail))
+            {
+                delete.DeleteDataFromDB(mail);
+                MessageBox.Show("This User Details has been deleted");
+                DeleteTextBox.Clear();
+            }
+            else
+                MessageBox.Show("Not Valid Mail");
+        }
+
+        private void TextBox_TextChanged_3(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+
+            //connection.Open();
+            //DataSet dataSet = new DataSet();
+            //using (SqlCommand cmd = new SqlCommand("sp_Select_users", connection))
+            //{
+            //    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            //    MessageBox.Show("your here");
+
+            //    SqlDataAdapter da = new SqlDataAdapter(cmd);
+            //    da.Fill(dataSet);
+            //    dataGrid1.DataContext = dataSet;
+            DataTable dt = new DataTable();
+            string mail, password;
+            connection.Open();
+            SqlCommand myCmd = new SqlCommand("sp_Select_users", connection);
+            SqlDataAdapter da = new SqlDataAdapter(myCmd);
+            SqlDataReader reader = myCmd.ExecuteReader();
+            if (reader != null && reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    mail = reader.GetString(reader.GetOrdinal("Mail"));
+                    password = reader.GetString(1);
+                    da.Fill(dt);
+                    MessageBox.Show(mail, password);
+
+                }
+
+            }
+
+            myCmd.CommandType = CommandType.StoredProcedure;
+            myCmd.ExecuteNonQuery();
+           
+            da.Fill(dt);
+            
+            //dataGrid1.DataSource = ds.Tables["Student"].DefaultView;
+        }
+
+        private void dataGrid1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+    }
+
---
+++ Server.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SqlInjectionWPF
+{
+    class Server
+    {
+       
+       
+        public static string ConfigurationManager { get; private set; }
+
+        public static string connectionString = "Data Source=DESKTOP-EDEVO0N\\SQLEXPRESS;Initial Catalog=SqlInjection;Integrated Security=True";
+        public static SqlConnection connection = new SqlConnection(connectionString);
+
+
+        public bool CheckIfExist(string mail)
+        {
+
+            connection.Open();
+            string query = "SELECT count(*) FROM MEMBERS WHERE [Mail] = '" + mail + "'";
+            bool exist = false;
+            SqlCommand checkMail = new SqlCommand(query, connection);
+            exist = (int)checkMail.ExecuteScalar() > 0;
+            if (exist)
+            {
+                connection.Close();
+                return true;
+               
+            }
+            connection.Close();
+            return false;
+
+        }
+
+        public void ConnectDataBase()
+        {
+
+
+            connection.Open();
+            MessageBox.Show("ok, your connect to DataBase...");
+        }
+        public void SelectFromDataBase()
+        {
+            string query = "Select * from MEMBERS";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            string mail, password;
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader != null && reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    mail = reader.GetString(reader.GetOrdinal("Mail"));
+                    password = reader.GetString(1);
+                    
+                    MessageBox.Show(mail, password);
+
+                }
+
+            }
+            connection.Close();
+        }
+
+        public void InsertToDataBase(string mail, string pass)
+        {
+            connection.Open();
+            string query = "INSERT INTO MEMBERS (Mail, Password) VALUES (@Mail, @Password)";
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.Add("@Mail", System.Data.SqlDbType.NVarChar);
+            command.Parameters.Add("@Password", System.Data.SqlDbType.NVarChar);
+            command.Parameters["@Mail"].Value = mail;
+            command.Parameters["@Password"].Value = pass;
+
+            int RowsAffected = command.ExecuteNonQuery();
+            MessageBox.Show("Insert Succes" + RowsAffected);
+
+            connection.Close();
+        }
+        public void UpdateData(string mail,string newmail, string pass)
+        {
+            connection.Open();
+            using (SqlCommand cmd =
+                new SqlCommand("UPDATE MEMBERS SET Mail=@Mail, Password=@Password" +
+                    " WHERE Mail= '" + mail + "'", connection))
+            {
+                
+                //add whatever parameters you required to update here
+                cmd.Parameters.Add("@Mail", System.Data.SqlDbType.NVarChar);
+                cmd.Parameters.Add("@Password", System.Data.SqlDbType.NVarChar);
+                cmd.Parameters["@Mail"].Value = newmail;
+                cmd.Parameters["@Password"].Value = pass;
+
+                int rows = cmd.ExecuteNonQuery();
+                Console.WriteLine(rows+"-rows affected");
+                
+            }
+        }
+        public void DeleteDataFromDB(string mail)
+        {
+            connection.Open();
+            SqlCommand cmd = new SqlCommand("DELETE FROM MEMBERS " +
+                     " WHERE Mail= '" + mail + "'", connection);
+            cmd.ExecuteNonQuery();
+            connection.Close();
+        }
+
+      
+    }
+}
+
